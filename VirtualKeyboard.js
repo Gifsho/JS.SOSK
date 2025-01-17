4cf5985e6173628c13e7debd09498a34d78f8639
Gifsho <padyod6@gmail.com>
2025-01-17 20:45:24 +0700

    up

--- VirtualKeyboard.js
+++ VirtualKeyboard.js
@@ -43,17 +43,32 @@ export class VirtualKeyboard {
     }
 
     decodeText(encryptedData) {
+      if (!encryptedData || !encryptedData.encrypted || !encryptedData.iv) {
+        if (encryptedData === "\t") {
+          return "\t";
+        } else {
+          return " ";
+        }
+      } 
+    
+      try {
         const decrypted = CryptoJS.AES.decrypt(
-            { ciphertext: CryptoJS.enc.Base64.parse(encryptedData.encrypted) },
-            CryptoJS.enc.Hex.parse(this.secretKey),
-            {
-                iv: CryptoJS.enc.Base64.parse(encryptedData.iv),
-                mode: CryptoJS.mode.CBC,
-                padding: CryptoJS.pad.Pkcs7
-            }
+          { ciphertext: CryptoJS.enc.Base64.parse(encryptedData.encrypted) },
+          CryptoJS.enc.Hex.parse(this.secretKey),
+          {
+            iv: CryptoJS.enc.Base64.parse(encryptedData.iv),
+            mode: CryptoJS.mode.CBC,
+            padding: CryptoJS.pad.Pkcs7
+          }
         );
         return decrypted.toString(CryptoJS.enc.Utf8);
+      } catch (error) {
+        console.error("Error decoding text:", error);
+        return " ";
+      }
     }
+    
+    
 
     getLayoutName(layout) {
         switch (layout) {
